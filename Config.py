1c1f66073faa9358589daca34e9da201b237eda4
SaugatModders <110078589+SaugatModders@users.noreply.github.com>
2022-12-13 22:14:34 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,7 +7,7 @@ class Config(object):
     STRING_SESSION = os.environ.get("STRING_SESSION", "1ApWapzMBu2MF_sdqxDsp5PE2Sta-xnPVcaZ_e-vKgKDxIzQl4EnCpu_yiNkNiwm4k7guNHL1VP10MPmrjPCsfohVlktPyzv2tHweZOabsjFwge80kNebZeiPe3v_KndDxkHLLP6YgjTPnPcJGCtjc8FN0nJc3dOw2HMpnmv9e6xuq7IyHTGUtLxM2BwYiCTioxMFBYqyJptoFTqJHRY_-1TC8z4_4MCKwhJIKhEDnKA78hcIcLOXjJnPOYukcZvhtUlvcpfDv1_JQ7RnzQRWFyIeDtmTT1p1dc6Vqos_AjB-MRs7P9M8lAblI_VvY3Vi_JoSnx9KKcLN3jAyJtqT3NAbKs0sCZY=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Shizuka_Music_Bot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Toxic_ka_bot")
     SUPPORT = os.environ.get("SUPPORT", "Mrnhgrx") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "Shizuka_update") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://te.legra.ph/file/6e0dab99a08982feb5215.jpg")
